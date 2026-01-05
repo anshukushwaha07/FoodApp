c239cf7acb31a0f913e49a82841e7c4b1dd76026
Anshu Kushwaha <anshu.kush007@gmail.com>
2026-01-05 17:40:31 +0530

    chore: update order model

---
+++ backend/src/models/Order.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+  food: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Food",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    restaurant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Restaurant",
+      required: true,
+    },
+
+    items: [orderItemSchema],
+
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "placed",
+        "confirmed",
+        "preparing",
+        "out_for_delivery",
+        "delivered",
+        "cancelled",
+      ],
+      default: "placed",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", orderSchema);
