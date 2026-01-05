45269ff23fb83f5f1170ecbc04d496580171db66
Anshu Kushwaha <anshu.kush007@gmail.com>
2026-01-05 17:40:35 +0530

    chore: update food routes

--- backend/src/routes/foodRoutes.js
+++ backend/src/routes/foodRoutes.js
@@ -8,13 +8,17 @@ import {
   deleteFood,
 } from "../controllers/foodController.js";
 
+import { protect } from "../middleware/authMiddleware.js";
+import { adminOnly } from "../middleware/adminMiddleware.js";
+
 const router = express.Router();
 
-router.post("/", createFood); // admin
 router.get("/", getAllFoods);
 router.get("/restaurant/:restaurantId", getFoodsByRestaurant);
 router.get("/:id", getFoodById);
-router.put("/:id", updateFood); // admin
-router.delete("/:id", deleteFood); // admin
+
+router.post("/", protect, adminOnly, createFood);
+router.put("/:id", protect, adminOnly, updateFood);
+router.delete("/:id", protect, adminOnly, deleteFood);
 
 export default router;
